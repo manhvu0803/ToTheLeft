b3194b44d1e131836986890670ad384736d17050
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2023-10-02 23:38:41 +0700

    Add level 17 (pickle jars) and level 18 (pasta)

---
+++ Assets/Scripts/Gameplay/LevelController/MultiTargetSlotLevelController.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTargetSlotLevelController : SlotLevelController
+{
+    [Header("Targets")]
+    [SerializeField]
+    private Interactable[] _targets;
+
+    protected readonly Dictionary<Transform, HashSet<Transform>> OccupantMap = new();
+
+    protected readonly Dictionary<Transform, Transform> SlotMap = new();
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        Utils.Fill(ref _targets);
+    }
+
+    public override bool UpdateSlot(Transform target, Transform slotTransform, bool isInteracting = false)
+    {
+        if (isInteracting)
+        {
+            return false;
+        }
+
+        // The target is removed from the stack
+        if (SlotMap.TryGetValue(target, out var oldSlot) && oldSlot != null && OccupantMap.TryGetValue(oldSlot, out var occupants))
+        {
+            occupants.Remove(target);
+
+            foreach (var occupant in occupants)
+            {
+                occupant.Translate(0, 0, -0.5f);
+            }
+        }
+
+        if (slotTransform != null)
+        {
+            if (!OccupantMap.TryGetValue(slotTransform, out occupants))
+            {
+                occupants = new HashSet<Transform>();
+                OccupantMap.Add(slotTransform, occupants);
+            }
+
+            foreach (var occupant in occupants)
+            {
+                occupant.Translate(0, 0, 0.5f);
+            }
+
+            occupants.Add(target);
+        }
+
+        SlotMap[target] = slotTransform;
+        return true;
+    }
+
+    public override float CompletionRate()
+    {
+        var correctCount = 0;
+
+        for (int i = 0; i < _targets.Length; ++i)
+        {
+            if (IsInCorrectSlot(_targets[i]))
+            {
+                correctCount++;
+            }
+        }
+
+        return (float)correctCount / _targets.Length;
+    }
+
+    private bool IsInCorrectSlot(Interactable target)
+    {
+        return SlotMap.TryGetValue(target.transform, out var slotTransform)
+            && slotTransform != null
+            && SlotTransforms.TryGetValue(slotTransform, out var slot)
+            && slot.IsTarget(target.transform);
+    }
+}
