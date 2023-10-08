b787c453e196f427cae520104fbc4cb3646559c0
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2023-10-08 22:53:16 +0700

    Fix level 23 (post-it circle)

---
+++ Assets/Scripts/Gameplay/LevelController/PostItCirleLevelController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PostItCirleLevelController : FreeSlotLevelController
+{
+    public override float CompletionRate()
+    {
+        var correctSlotCount = 0;
+        var correctRotationCount = 0;
+
+        foreach (var slot in Slots)
+        {
+            if (OccupantMap.TryGetValue(slot.transform, out var occupant)
+                && slot.IsTarget(occupant))
+            {
+                correctSlotCount++;
+
+                if (Mathf.Abs(occupant.transform.eulerAngles.z - slot.transform.eulerAngles.z) <= 0.01f)
+                {
+                    correctRotationCount++;
+                }
+            }
+        }
+
+        return (float)(correctSlotCount + correctRotationCount) / (Slots.Length * 2);
+    }
+}
